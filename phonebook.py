db6cc18e3ce5f7c7860163ee3561b243f45cb5fb
khiljaekang <64455713+khiljaekang@users.noreply.github.com>
2021-03-04 00:31:16 +0900

    전화번호 목록

---
+++ phonebook.py
@@ -0,0 +1,20 @@
+def solution(phone_book):
+    answer = True 
+    
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] in phone_book[i+1]:
+            answer = False
+            break
+
+    return answer
+
+
+
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
