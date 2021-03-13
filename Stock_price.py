6eb5f1e584794a2d340084f1c4386dac294986f6
khiljaekang <64455713+khiljaekang@users.noreply.github.com>
2021-03-13 23:41:29 +0900

    주식가격

---
+++ Stock_price.py
@@ -0,0 +1,22 @@
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
+
+
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
